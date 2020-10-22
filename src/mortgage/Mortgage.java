5e65ef4c1faf66f19893bd51cb83dd09be5ed408
Calvin Loh <calvin_loh138@yahoo.com>
2020-10-22 14:00:27 +0700

    Version 2, (Added a while loop statement to catch value that is wrong) (Next is to clean)

--- src/mortgage/Mortgage.java
+++ src/mortgage/Mortgage.java
@@ -32,15 +32,34 @@ public class Mortgage {
         int numberOfPayment;
         double mortgage;
         
-        System.out.print("Principal : ");
-        principal = sc.nextInt();
+        while(true){
+            System.out.print("Principal : $ ");
+            principal = sc.nextInt();
+            if(principal >= 10000 && principal <= 1000000){
+                break;
+            }
+            System.out.println("Enter a value between $10.000 and $1.000.000");
+        }
         
-        System.out.print("Annual Interest Rate : ");
-        annualInterestRate = sc.nextFloat();
+        while(true){
+            System.out.print("Annual Interest Rate : ");
+            annualInterestRate = sc.nextFloat();
+            if(annualInterestRate >= 1 && annualInterestRate <= 30){
+                break;
+            }
+            System.out.println("Please enter a value between 1 and 30");
+        }
         monthlyInterestRate = annualInterestRate / PERCENT / MONTH_IN_YEAR;
         
-        System.out.print("Period (Years) : ");
-        period = sc.nextByte();
+        
+        while(true){
+            System.out.print("Period (Years) : ");
+            period = sc.nextByte();
+            if(period >= 1 && period <= 30){
+                break;
+            }
+            System.out.println("Please enter a value more than 0 and less than or equals to 30");
+        }
         numberOfPayment = period * MONTH_IN_YEAR;
         
         
@@ -55,5 +74,4 @@ public class Mortgage {
         
         System.out.println("Mortgage : " + mortgageFormatted);
     }
-    
 }
